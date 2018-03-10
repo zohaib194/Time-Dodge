23392b5835f4f21420c97715d44aa1093a7d1c35
kentwh <kentwh@stud.ntnu.no>
2018-03-10 17:18:23 +0100

    Added a new class 'CustomCanvas' made it draw a black rectangle to the view/canvas (as the background), pluss added Logging tags in 'CustomCanvas.java'. Added a new simple container class 'CollisionBox' for holding collision box information in 'CollisionBox.java'

---
+++ app/src/main/java/no/xillez/kentwh/mobilelab3/CollisionBox.java
@@ -0,0 +1,18 @@
+package no.xillez.kentwh.mobilelab3;
+
+/**
+ * Created by kent on 10.03.18.
+ */
+
+public class CollisionBox
+{
+    public float x, y, z, w;
+
+    public CollisionBox(float x, float y, float z, float w)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+}
---
+++ app/src/main/java/no/xillez/kentwh/mobilelab3/CustomCanvas.java
@@ -0,0 +1,64 @@
+package no.xillez.kentwh.mobilelab3;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by kent on 10.03.18.
+ */
+
+public class CustomCanvas extends View
+{
+    private static final String LOG_TAG_INFO = "Xillez_CustomCanvas [INFO]";
+    private static final String LOG_TAG_WARN = "Xillez_CustomCanvas [WARN]";
+
+    private ShapeDrawable background;
+    private CollisionBox backCollBox;
+
+    private Point wSize;
+    private int MARGIN = 5;
+
+    public CustomCanvas(Context context)
+    {
+        super(context);
+        wSize = new Point();
+
+        // Get screen dimensions
+        wSize.set(context.getResources().getDisplayMetrics().widthPixels, context.getResources().getDisplayMetrics().heightPixels);
+
+        // Setup background
+        Log.i(LOG_TAG_INFO, "Building background!");
+        makeBackground();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        // Draw background
+        Log.i(LOG_TAG_INFO, "Drawing on canvas!");
+        background.draw(canvas);
+
+        // TODO: DRAW BALL!
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    private void makeBackground()
+    {
+        // Make new rectangle shape, set it's color, position and collision box
+        background = new ShapeDrawable(new RectShape());
+        background.getPaint().setColor(Color.DKGRAY);
+        background.setBounds(MARGIN, MARGIN, wSize.x - MARGIN, wSize.y - MARGIN);
+        backCollBox = new CollisionBox(MARGIN, MARGIN, wSize.x - MARGIN, wSize.y - MARGIN);
+    }
+}
