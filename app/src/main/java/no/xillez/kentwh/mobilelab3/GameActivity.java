82bf187f2bb72deb782e269f8890e0f63ecf52b2
kentwh <kentwh@stud.ntnu.no>
2018-03-10 17:14:51 +0100

    Added Logging tag strings pluss made view base upon upcomming class 'CustCanvas' in 'GameActivity.java'

--- app/src/main/java/no/xillez/kentwh/mobilelab3/GameActivity.java
+++ app/src/main/java/no/xillez/kentwh/mobilelab3/GameActivity.java
@@ -3,13 +3,25 @@ package no.xillez.kentwh.mobilelab3;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class GameActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG_INFO = "Xillez_GameActivity [INFO]";
+    private static final String LOG_TAG_WARN = "Xillez_GameActivity [WARN]";
+
+    private CustomCanvas customCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+
+        // Set view
+        Log.i(LOG_TAG_INFO, "Setting view!");
+        customCanvas = new CustomCanvas(getApplicationContext());
+        setContentView(customCanvas);
+
+        Log.i(LOG_TAG_INFO, "Setting screen orientation!");
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
