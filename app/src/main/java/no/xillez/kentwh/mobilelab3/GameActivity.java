e6b41d268e0f9d10a3c5071886443b5610929811
kentwh <kentwh@stud.ntnu.no>
2018-03-11 16:39:42 +0100

    Added commented debug code and Logging in 'GameActivity.java'

--- app/src/main/java/no/xillez/kentwh/mobilelab3/GameActivity.java
+++ app/src/main/java/no/xillez/kentwh/mobilelab3/GameActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class GameActivity extends AppCompatActivity
+public class GameActivity extends AppCompatActivity// implements SensorEventListener
 {
 
     private static final String LOG_TAG_INFO = "Xillez_GameActivity [INFO]";
@@ -20,10 +20,15 @@ public class GameActivity extends AppCompatActivity
     private SensorManager sensorManager;
     public Sensor sensor;
 
+    /*private TextView xView;
+    private TextView yView;
+    private TextView zView;*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        Log.i(LOG_TAG_INFO, "App starting up!");
 
         // Set view
         Log.i(LOG_TAG_INFO, "Setting view!");
@@ -34,19 +39,26 @@ public class GameActivity extends AppCompatActivity
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         // Get accelerometer
+        Log.i(LOG_TAG_INFO, "Finding acceleration sensor (accelerometer)!");
         sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        Log.i(LOG_TAG_INFO, "Tying to register sensor!");
         sensorManager.registerListener(gameCanvas, sensor, SensorManager.SENSOR_DELAY_GAME);
 
         // Give it to Canvas
+        Log.i(LOG_TAG_INFO, "Passing on sensor to canvas!");
         gameCanvas.setSensor(sensor);
+
+        /*this.xView = findViewById(R.id.game_textview01);
+        this.yView = findViewById(R.id.game_textview02);*/
     }
 
     @Override
     protected void onPause()
     {
         super.onPause();
+        Log.i(LOG_TAG_INFO, "App paused, un-registering sensor listener");
         sensorManager.unregisterListener(gameCanvas);
     }
 
@@ -54,6 +66,20 @@ public class GameActivity extends AppCompatActivity
     protected void onResume()
     {
         super.onResume();
+        Log.i(LOG_TAG_INFO, "App un-paused, registering sensor listener");
         sensorManager.registerListener(gameCanvas, sensor, SensorManager.SENSOR_DELAY_GAME);
     }
+
+    /*@Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        xView.setText("" + event.values[0]);
+        yView.setText("" + event.values[1]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
+    }*/
 }
