aee92c4cb31c97f55724f79242819e5595f12fa6
kentwh <kentwh@stud.ntnu.no>
2018-03-11 12:47:35 +0100

    Renamed 'CustomCanvas' to 'GameCanvas' (both file and variable). Replaced 'onRestart' with 'onResume' and added fetching accelerometer sensor in 'GameActivity.java'

--- app/src/main/java/no/xillez/kentwh/mobilelab3/GameActivity.java
+++ app/src/main/java/no/xillez/kentwh/mobilelab3/GameActivity.java
@@ -1,6 +1,9 @@
 package no.xillez.kentwh.mobilelab3;
 
+import android.app.Service;
 import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,7 +14,11 @@ public class GameActivity extends AppCompatActivity
     private static final String LOG_TAG_INFO = "Xillez_GameActivity [INFO]";
     private static final String LOG_TAG_WARN = "Xillez_GameActivity [WARN]";
 
-    private CustomCanvas customCanvas;
+    private GameCanvas gameCanvas;
+
+    // SensorManagers
+    private SensorManager sensorManager;
+    public Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,22 +27,33 @@ public class GameActivity extends AppCompatActivity
 
         // Set view
         Log.i(LOG_TAG_INFO, "Setting view!");
-        customCanvas = new CustomCanvas(getApplicationContext());
-        setContentView(customCanvas);
+        gameCanvas = new GameCanvas(getApplicationContext());
+        setContentView(gameCanvas);
 
         Log.i(LOG_TAG_INFO, "Setting screen orientation!");
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        // Get accelerometer
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener(gameCanvas, sensor, SensorManager.SENSOR_DELAY_GAME);
+
+        // Give it to Canvas
+        gameCanvas.setSensor(sensor);
     }
 
     @Override
     protected void onPause()
     {
         super.onPause();
+        sensorManager.unregisterListener(gameCanvas);
     }
 
     @Override
-    protected void onRestart()
+    protected void onResume()
     {
-        super.onRestart();
+        super.onResume();
+        sensorManager.registerListener(gameCanvas, sensor, SensorManager.SENSOR_DELAY_GAME);
     }
 }
