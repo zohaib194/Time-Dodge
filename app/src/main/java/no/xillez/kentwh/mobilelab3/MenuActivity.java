e38ab9bff921dff16547dfad2be40079508c6fd3
Eldar Hauge Torkelsen <eldarht@stud.ntnu.no>
2018-04-06 15:09:27 +0200

    Added Menu for mavigation

---
+++ app/src/main/java/no/xillez/kentwh/mobilelab3/MenuActivity.java
@@ -0,0 +1,30 @@
+package no.xillez.kentwh.mobilelab3;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity implements MenuNavigationFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    public void actionStartGame(View view){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void actionStartSetting(View view){
+        // TODO: Start settings activity
+    }
+}
