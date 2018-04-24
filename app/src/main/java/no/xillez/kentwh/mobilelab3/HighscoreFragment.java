cebca5aa8a127fee847d5b9fa9b1f799a5bf200a
zohaib <zohaibb@stud.ntnu.no>
2018-04-24 14:16:43 +0200

    changed name convention

--- app/src/main/java/no/xillez/kentwh/mobilelab3/HighscoreFragment.java
+++ app/src/main/java/no/xillez/kentwh/mobilelab3/HighscoreFragment.java
@@ -63,10 +63,10 @@ public class HighscoreFragment extends Fragment{
         this.root = FirebaseDatabase.getInstance().getReference().getRoot();
 
         // UI setup.
-        this.highscoreListView = view.findViewById(R.id.LW1);
-        this.button1 = view.findViewById(R.id.B1);
-        this.button2 = view.findViewById(R.id.B2);
-        this.button3 = view.findViewById(R.id.B3);
+        this.highscoreListView = view.findViewById(R.id.frag_highscore_listview01);
+        this.button1 = view.findViewById(R.id.frag_highscore_button01);
+        this.button2 = view.findViewById(R.id.frag_highscore_button02);
+        this.button3 = view.findViewById(R.id.frag_highscore_button03);
 
         // Array lists and setup array list adapter
         this.scoreList = new ArrayList<>();
@@ -92,7 +92,6 @@ public class HighscoreFragment extends Fragment{
 
         onDBUpdate();
 
-        // TODO : Sorting the arrays
         // Top 5 button on click listener
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -115,8 +114,6 @@ public class HighscoreFragment extends Fragment{
                 }
             }
         });
-
-        // TODO : fix button 2 and 3 to show correct amount of items.
 
         // Top 10 button on click listener
         button2.setOnClickListener(new View.OnClickListener(){
