8a9edeadc899d1d24ea8fd188c97133764ce95c6
zohaib <zohaibb@stud.ntnu.no>
2018-04-24 09:34:53 +0200

    Highscore buttons are done and scroll bar is fixed

--- app/src/main/java/no/xillez/kentwh/mobilelab3/HighscoreFragment.java
+++ app/src/main/java/no/xillez/kentwh/mobilelab3/HighscoreFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,8 +22,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HighscoreFragment extends Fragment{
@@ -32,8 +38,10 @@ public class HighscoreFragment extends Fragment{
     private Button button1;
     private Button button2;
     private Button button3;
-    private ArrayList<String> scoreList;
+    private ArrayList<Long> scoreList;
+    private ArrayList<Long> adapterScore;
     private ArrayList<String> userList;
+    private ArrayList<String> adapterUser;
     private ArrayAdapter adapter;
     private OnFragmentInteractionListener mListener;
 
@@ -41,32 +49,146 @@ public class HighscoreFragment extends Fragment{
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the view.
+        View view = inflater.inflate(R.layout.fragment_highscore, container, false);
+
+        // DB connection.
+        this.root = FirebaseDatabase.getInstance().getReference().getRoot();
+
+        // UI setup.
+        this.highscoreListView = view.findViewById(R.id.LW1);
+        this.button1 = view.findViewById(R.id.B1);
+        this.button2 = view.findViewById(R.id.B2);
+        this.button3 = view.findViewById(R.id.B3);
+
+        // Array lists and setup array list adapter
+        this.scoreList = new ArrayList<>();
+        this.userList = new ArrayList<>();
+        this.adapterScore = new ArrayList<>();
+        this.adapterUser = new ArrayList<>();
+
+        this.adapter = new ArrayAdapter<String>(getActivity(), R.layout.highscore_layout, R.id.text1, adapterUser){
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView t1 = view.findViewById(R.id.text1);
+                TextView t2 = view.findViewById(R.id.text2);
+
+                t1.setText(adapterUser.get(position));
+                t2.setText(adapterScore.get(position).toString());
+
+                return view;
+            }
+        };
+        this.highscoreListView.setAdapter(adapter);
+
         onDBUpdate();
 
-        button1.
+        // TODO : Sorting the arrays
+        // Top 5 button on click listener
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (scoreList.size() - 1 > 5) {
+                    Long[] scoreArray = new Long[5];
+                    String[] userArray = new String[5];
+                    userArray = userList.subList(0, 5).toArray(userArray);
+                    scoreArray = scoreList.subList(0, 5).toArray(scoreArray);
+
+                    adapterScore.clear();
+                    adapterUser.clear();
+
+                    for(int i = 0; i < userArray.length; i++){
+                        adapterUser.add(userArray[i]);
+                        adapterScore.add(scoreArray[i]);
+                    }
+
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        // TODO : fix button 2 and 3 to show correct amount of items.
+
+        // Top 10 button on click listener
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (scoreList.size() - 1 > 10) {
+
+                    Long[] scoreArray = new Long[10];
+                    String[] userArray = new String[10];
+                    userArray = userList.subList(0, 10).toArray(userArray);
+                    scoreArray = scoreList.subList(0, 10).toArray(scoreArray);
+
+                    adapterScore.clear();
+                    adapterUser.clear();
+
+                    for(int i = 0; i < userArray.length; i++){
+                        adapterUser.add(userArray[i]);
+                        adapterScore.add(scoreArray[i]);
+                    }
+
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        // Overall button on click listener
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                adapterScore.clear();
+                adapterUser.clear();
+                for(int i = 0; i < userList.size(); i++){
+                    adapterUser.add(userList.get(i));
+                    adapterScore.add(scoreList.get(i));
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
+        return view;
     }
 
     public void debugfunction(){
-        for(int i = 0; i < 10; i++){
-            scoreList.add("yolo : " + i);
+        scoreList.clear();
+        for(long i = 0; i < 20; i++){
+            scoreList.add(i);
 
             Map<String, Object> map = new HashMap<>();
-            map.put("u", "YOLO");
-            map.put("m", scoreList.get(i));
+            map.put("u", "zohaib" + i);
+            map.put("s", i);
             root.child("score").push().updateChildren(map);
         }
+        adapter.notifyDataSetChanged();
     }
 
     private void onDBUpdate(){
-        this.root = FirebaseDatabase.getInstance().getReference().getRoot();
-
         root.child("score").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
-                    scoreList.add(dataSnapshot1.getKey() + " : " + dataSnapshot1.getValue());
+                    if(dataSnapshot1.getKey().equals("u")){
+                        userList.add((String) dataSnapshot1.getValue());
+                        adapterUser.add((String) dataSnapshot1.getValue());
 
+                    } else {
+                        scoreList.add((Long) dataSnapshot1.getValue());
+                        adapterScore.add((Long) dataSnapshot1.getValue());
+
+                    }
                 }
+                sort(scoreList, userList, 0, scoreList.size() - 1);
+                sort(adapterScore, adapterUser, 0, adapterScore.size() - 1);
+                adapter.notifyDataSetChanged();
             }
 
             @Override
@@ -91,43 +213,41 @@ public class HighscoreFragment extends Fragment{
         });
     }
 
-    private void onClick(){
 
-    }
+    // sort the lists
+    private void sort(List<Long> sList, List<String> uList, int low, int high){
+        int i = low, j = high;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Long pivot = sList.get(low + (high-low)/2);
 
-        View view = inflater.inflate(R.layout.fragment_highscore, container, false);
+        while (i <= j) {
 
-        this.highscoreListView = view.findViewById(R.id.LW1);
-        this.button1 = view.findViewById(R.id.B1);
-        this.button2 = view.findViewById(R.id.B2);
-        this.button3 = view.findViewById(R.id.B3);
-
-        this.scoreList = new ArrayList<>();
-        this.userList = new ArrayList<>();
-        this.adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, scoreList){
-            @NonNull
-            @Override
-            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-                TextView t1 = view.findViewById(android.R.id.text1);
-
-                t1.setText(scoreList.get(position));
-
-                return view;
+            while (sList.get(i) > pivot) {
+                i++;
             }
-        };
+            while (sList.get(j) < pivot) {
+                j--;
+            }
 
-        this.highscoreListView.setAdapter(adapter);
+            if (i <= j) {
+                // swap
+                Long temp = sList.get(i);
+                sList.set(i, sList.get(j));
+                sList.set(j, temp);
 
-        debugfunction();
+                String temp1 = uList.get(i);
+                uList.set(i, uList.get(j));
+                uList.set(j, temp1);
 
-        adapter.notifyDataSetChanged();
+                i++;
+                j--;
+            }
+        }
 
-        return view;
+        if (low < j)
+            sort(sList, uList, low, j);
+        if (i < high)
+            sort(sList, uList, i, high);
     }
 
     /**
