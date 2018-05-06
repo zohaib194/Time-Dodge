3706db595871d190b2c07898c1506fede292e0a1
kentwh <kentwh@stud.ntnu.no>
2018-05-06 18:03:47 +0200

    Refactored 'CollisionState.java'

--- app/src/main/java/no/xillez/kentwh/mobilelab3/CollisionState.java
+++ app/src/main/java/no/xillez/kentwh/mobilelab3/CollisionState.java
@@ -1,13 +1,17 @@
 package no.xillez.kentwh.mobilelab3;
 
-/**
- * Created by kent on 12.03.18.
- */
-
 public class CollisionState
 {
     public boolean left, top, bottom, right;
 
+    /**
+     * Constructor for 'CollisionState' class. This is a simple container class.
+     *
+     * @param left - the left bounds of the object.
+     * @param top - the top bounds of the object.
+     * @param bottom - the bottom bounds of the object.
+     * @param right - the rights bounds of the object.
+     */
     public CollisionState(boolean left, boolean top, boolean bottom, boolean right)
     {
         this.left = left;
